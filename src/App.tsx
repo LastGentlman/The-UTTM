8a4e43dc8fcc89a516da12b83d529d54d5104e4b
Rodrigo <gokusan@keemail.me>
2022-11-07 12:14:37 -0600

    :zap: Project Landing Page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <div className="container">
+      <h1>U Task Mngr</h1>
+      <br/>
+      <h3>Project under development</h3>
+      <h4>Stay tunned follow me on <a href='https://twitter.com/1ngRoy'>Twitter</a></h4>
+  </div>
+  )
+}
+
+export default App
