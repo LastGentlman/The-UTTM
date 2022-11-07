3b1b207f3e7d1abdf8521be8dda02af917fba65b
Rodrigo <gokusan@keemail.me>
2022-11-07 12:14:39 -0600

    :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ function App() {
 
   return (
   <div className="container">
-      <h1>U Task Mngr</h1>
-      <br/>
+      <h1>The UTTM</h1>
       <h3>Project under development</h3>
       <h4>Stay tunned follow me on <a href='https://twitter.com/1ngRoy'>Twitter</a></h4>
   </div>
