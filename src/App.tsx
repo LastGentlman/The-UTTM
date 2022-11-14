295f06e094ce7ecacbe72a807a70e4941a6793d8
Rodrigo <gokusan@keemail.me>
2022-11-14 07:51:13 -0600

    :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Nav from './modules/Nav'
 
 function App() {
 
   return (
-  <div className="container">
-      <h1>The UTTM</h1>
-      <h3>Project under development</h3>
-      <h4>Stay tunned follow me on <a href='https://twitter.com/1ngRoy'>Twitter</a></h4>
-  </div>
+    <>
+      <Nav/>
+    </>
   )
 }
 
---
+++ src/modules/Nav.tsx
@@ -0,0 +1,7 @@
+import {} from '../styles/Nav.style.css'
+
+function Nav() {
+
+}
+
+export default Nav()
